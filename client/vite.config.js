c8f95a258c456ba789581856351f61c1c3ea443b
hhusein-alt <gm1080rr@gmail.com>
2025-05-09 21:21:28 +0400

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bookshelf/',
+  base: '/bookshelfes/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
     rollupOptions: {
