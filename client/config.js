550dee319f1c46df1c5bc0c5e155e15bb52f766a
hhusein-alt <gm1080rr@gmail.com>
2025-05-22 19:10:50 +0400

    refactor: remove hardcoded Supabase credentials, use env variables

--- client/config.js
+++ client/config.js
@@ -1,5 +1,8 @@
+export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+export const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 const config = {
     supabaseUrl: 'https://vzyjahpxhcmkwffrlybs.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eWphaHB4aGNta3dmZnJseWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NDMxMDUsImV4cCI6MjA2MjIxOTEwNX0.rI0LLJ5QCtk4CXbSeVIxDqWYeyoTkjyBwgRcJ_RxVyE',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6eWphaHB4aGNta3dmZnJseWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NDMxMDUsImV4cCI6MjA2MjIxOTEwNX0.rI0LLJ5QCtk4CXbSeVIxDqWYeyoTkjyBwgRcJ_RxVyE',    
     linktreeUrl: 'https://linktr.ee/BookShelvzz'
 }; 
